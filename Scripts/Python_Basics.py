986ceb7ba052912bba97a605d7a1579ea742ffc0
Naa Odey Solomon <45242641+naaodey@users.noreply.github.com>
2025-10-06 13:24:49 +0000

    Update Python_Basics.py

--- Scripts/Python_Basics.py
+++ Scripts/Python_Basics.py
@@ -1,2 +1,10 @@
-import numpy as np
+print(type(int(6.5) + True + bool("python")))
+
+bool("python") ### This is returns True because is not an empty string
+
+bool("") ### Returns false because it an empty string
+
+name = "Bob"
+surname = " Smith"
+print(name +  surname)
 
